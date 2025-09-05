1262eab8244d3cd5261754e20065544d74631340
suman <sumanmukherjee2001@gmail.com>
2025-09-05 19:10:58 +0530

    chore: improved benchmark test

--- queue/queue_benchmark_test.go
+++ queue/queue_benchmark_test.go
@@ -7,53 +7,22 @@ import (
 )
 
 func BenchmarkQueueQps(b *testing.B) {
-	topic := NewTopic("test", TopicConfig{
-		AckTimeout: 30 * time.Second,
-		MaxRetries: 3,
-	})
+    topic := NewTopic("test", TopicConfig{
+        AckTimeout: 30 * time.Second,
+        MaxRetries: 3,
+    })
 
-	b.ResetTimer()
+    b.ResetTimer()
 
-	go func() {
-		for i := 0; i < b.N; i++ {
-			topic.Enqueue(fmt.Sprintf("message %d", i))
-		}
-	}()
+    for i := 0; i < b.N; i++ {
+        topic.Enqueue(fmt.Sprintf("message %d", i))
 
-	for i := 0; i < b.N; i++ {
-		msg, err := topic.Dequeue()
-		if err == nil {
+        msg, err := topic.Dequeue()
+        if err != nil {
+            b.Fatalf("Dequeue failed at iteration %d: %v", i, err)
+        }
 
-			topic.Acknowledge(msg.ID)
-		}
-	}
+        topic.Acknowledge(msg.ID)
+    }
 }
 
-func BenchmarkQueueProdCons(b *testing.B) {
-	q := NewTopic("bench", TopicConfig{})
-
-	done := make(chan struct{})
-
-	// Consumer goroutine
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			default:
-				msg, err := q.Dequeue()
-				if err == nil {
-					q.Acknowledge(msg.ID)
-				}
-			}
-		}
-	}()
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		q.Enqueue("data")
-	}
-
-	close(done)
-}
